016f9bfa62bcc8519e85c702e3ea64a102b75caf
tumaolin94 <524561223@qq.com>
2017-09-18 01:58:56 -0700

    new migration

--- Ex_Files_Django_1_Blog/Exercise Files/Chapter 1/mysite/blog/models.py
+++ Ex_Files_Django_1_Blog/Exercise Files/Chapter 1/mysite/blog/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-# Create your models here.
+from django.core.urlresolvers import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
-
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, related_name='blog_posts')
@@ -17,9 +23,17 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # The Dahl-specific manager.
+
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.strftime('%m'),
+                                                 self.publish.strftime('%d'),
+                                                 self.slug])
