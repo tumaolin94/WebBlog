e4200f25ea381fca293dac3d05a989f47245563c
tumaolin94 <524561223@qq.com>
2017-10-23 23:10:12 -0700

    Sharing posts by email

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'tumaolin.richard@gmail.com'
+EMAIL_HOST_PASSWORD = 'qweasd123asd'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
